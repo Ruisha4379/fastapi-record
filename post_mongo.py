513848d74dc7126a49e74d00e6f41f71879b8efd
Ruisha4379 <43809989+Ruisha4379@users.noreply.github.com>
2022-10-21 14:14:33 +0800

    Update post_mongo.py

--- post_mongo.py
+++ post_mongo.py
@@ -12,23 +12,25 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 client = MongoClient()
-db = "sw_data"
-col = "messages" # database collection
+db = "vpp_data"
+col = "create_vpp" # database collection
 # Message class defined in Pydantic
-class Message(BaseModel):
-    channel: str
-    author: str
-    text: str
+class VPP(BaseModel):
+    name: str
+    vpp_id: str
+    status: str
+    market_id: str
+    market_type: str
 # Instantiate the FastAPI
 app = FastAPI()
 
-@app.post("/post_message", status_code=status.HTTP_201_CREATED)
-def post_message(message: Message):
+@app.post("/data/vpptree", status_code=status.HTTP_201_CREATED)
+def post_vpp(create_vpp: VPP):
     """Post a new message to the specified channel."""
     with MongoClient() as client:
         msg_col = client[db][col]
-        insert_result = msg_col.insert_one(message.dict())
-        msg_content = Message(**message.dict())
+        insert_result = msg_col.insert_one(create_vpp.dict())
+        msg_content = VPP(**create_vpp.dict())
         ack = insert_result.acknowledged
         #print (msg_col)
         print(insert_result)
@@ -41,6 +43,7 @@ def post_message(message: Message):
 if __name__ == '__main__':
     #it will run on port 8000 and it can receive traffic from anywhere
     uvicorn.run(app,debug=True,port=8000,host='0.0.0.0')
+    
 # How to varify the data input from the POST endpoint is successfully insert into the mongoDB:     
 # Step 1: Tyep `$ ./mongod` at the root path
 # Step 2: Type `mongto` in the environment path to run in the mongo shell 
