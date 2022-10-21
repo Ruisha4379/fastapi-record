fe347a38af2257d582db3d8ec929e4a1e4e36200
Ruisha4379 <43809989+Ruisha4379@users.noreply.github.com>
2022-10-21 16:16:50 +0800

    Update models.py
    models map the data format in MongoDB. The class name and the keys, the format of values all should be aligned between the models defined class "Create_vpp" and MongoDB collection "create_vapp".
    The class use of capital letter in "Create_vpp" is acceptable as long as the letter content match.

--- models.py
+++ models.py
@@ -1,9 +1,15 @@
 """
-Define the data format in the function Employee
+Linkup FASTAPI with MongoDB
 """
 from mongoengine import Document, StringField,IntField, ListField
-
-class Employee(Document):
-    name = StringField(max_len=100)
-    age = IntField()
-    teams= ListField()
+# Need to map to the mongoDB schema for db created "vpp_data"
+# The class name should match with the db collection name which is "create_vpp"
+class Create_vpp(Document): 
+    # name = StringField(max_len=100)
+    # age = IntField()
+    # teams= ListField()
+    name = StringField
+    vpp_id = StringField
+    status= StringField
+    market_id= StringField
+    market_type= StringField
