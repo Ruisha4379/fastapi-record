29af73fcefe2b538ed648e900df1e42ac6baf2ba
Ruisha4379 <43809989+Ruisha4379@users.noreply.github.com>
2022-10-21 15:36:02 +0800

    Create get_mongo.py

---
+++ get_mongo.py
@@ -0,0 +1,34 @@
+"""
+Get data created from MongoDB database "vpp_data" collection of "create_vpp".
+"""
+from pymongo import MongoClient
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+from models import Create_vpp
+from mongoengine import connect #connect to mongodb 
+from typing import List
+import uvicorn
+import json
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+#client = MongoClient()
+#db = "vpp_data"
+#col = "create_vpp" # database collection
+
+app = FastAPI() 
+connect(db = "vpp_data", host="localhost", port = 27017)
+
+
+@app.get("/query/vpptree")
+def get_vpp():
+    vpp = json.loads(Create_vpp.objects().to_json()) # return all the employee docs
+    #vpp = Create_vpp.objects()
+    print(vpp)
+    #employees_list = json.loads(employees)
+    print(type(vpp)) # print the types of var
+    return {"VPP created":  vpp} # retun to the client
+    
+if __name__ == '__main__':
+    #it will run on port 3000 and it can receive traffic from anywhere
+    uvicorn.run(app,debug=True,port=8000,host='0.0.0.0')
