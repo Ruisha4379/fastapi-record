bbb24a1a8cd316e83104f001dc85c51ba6467e62
Ruisha4379 <43809989+Ruisha4379@users.noreply.github.com>
2022-11-02 16:45:50 +0800

    Update models_hie.py

--- models_hie.py
+++ models_hie.py
@@ -1,30 +1,41 @@
 """
-Date: 2022-10-31
+Date: 2022-11-02
 Define the data format for the GET endpoint to retrieve data stored at MongoDB
 """
 from mongoengine import Document, StringField,IntField, ListField
 # Need to map to the mongoDB schema for db created "vpp_data"
 # The class name should match with the db collection name which is "create_vpp"
+
 class Vpp_metadata(Document): 
     # name = StringField(max_len=100)
     # age = IntField()
     # teams= ListField()
-    Name = StringField
-    VPP_ID = StringField
-    Market_Type = StringField
-    Control_Area = StringField
-    Communication_Protocol = StringField
-    Flex_Up_Cost = StringField
-    Flex_Down_Cost = StringField
-    Flex_Up_Capacity = StringField
-    Flex_Down_Capacity = StringField 
-    Description = StringField
-    Sites = StringField
+    Name                  = StringField
+    VppID                 = StringField
+    MarketType            = StringField
+    ControlArea           = StringField
+    CommunicationProtocol = StringField
+    FlexUpCost            = StringField
+    FlexDownCost          = StringField
+    FlexUpCapacity        = StringField
+    FlexDownCapacity      = StringField 
+    Description           = StringField
+    #Sites                 = StringField
 
 class Sites_metadata(Document):
-    Name = StringField
-    Site_ID = StringField
-    Site_address = StringField
-    Assigned_VPP = StringField
-    Description = StringField
-    Market_resource = StringField
+    Name           = StringField
+    SiteID         = StringField
+    SiteAddress    = StringField
+    AssignedVPP    = StringField
+    Description    = StringField
+    #MarketResource = StringField
+
+  
+class MarketResource_metadata(Document):
+    Name           = StringField
+    ResourceID     = StringField
+    ResourceType   = StringField
+    AssignedVPP   = StringField
+    AssignedSite   = StringField
+    Description    = StringField
+    
